ab7fcce76707304ba6fa23245bc3482a492324e2
Yves0708 <yvesloveisis@gmail.com>
2015-08-16 14:39:32 +0800

    Bmi1.1

--- BmiCal/src/com/yves/bmical/MainActivity.java
+++ BmiCal/src/com/yves/bmical/MainActivity.java
@@ -2,6 +2,7 @@ package com.yves.bmical;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
@@ -13,11 +14,16 @@ public class MainActivity extends Activity {
 	EditText et_weight;
 	Button btn01;
 	TextView tv_result;
-	TextView tv_suggeest;
+	TextView tv_suggest;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+		et_height = (EditText)findViewById(R.id.et_height);
+		et_weight = (EditText)findViewById(R.id.et_weight);
+		tv_result = (TextView)findViewById(R.id.tv_result);
+		tv_result.setText(Html.fromHtml("歡迎大家使用<big><font color=\"#FF0000\"><b>BMI 計算器</b></font></big>"));//改變某段文字格式
+		tv_suggest = (TextView)findViewById(R.id.tv_suggest);
+		btn01 =(Button)findViewById(R.id.btn01);
 	}
 }
